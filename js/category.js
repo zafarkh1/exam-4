e9a53f0838ce0a93b8882fedabeaf2413810f389
zafarkh <hatamovzafar7070@gmail.com>
2023-03-14 22:00:25 +0500

    ff

--- js/category.js
+++ js/category.js
@@ -1,11 +1,20 @@
 const cardWrapper = document.querySelector(".card-wrapper");
+const input = document.getElementById("search");
 let key = "1e31a6fe70964dccbb73ff1df7541bb0";
 let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${key}`;
 
 fetch(url).then((response) => response.json()).then((data) => {
+ 
 	const articles = data.articles
 	const filteredArticles = articles.filter((element) => {
-		return element.urlToImage && element.urlToImage !== null && element.author
+		console.log(element);
+		return (
+      element.urlToImage &&
+      element.urlToImage !== null &&
+      element.author &&
+      element.author &&
+      element.author.length <= 13
+    );
 	})
 	const render = filteredArticles.map((element) => {
 		return `
@@ -21,3 +30,13 @@ fetch(url).then((response) => response.json()).then((data) => {
   })
 	cardWrapper.innerHTML = render.join("")
 })
+
+
+	// let url;
+  // const searchTerm = document.querySelector("#search").value;
+	// if(searchTerm) {
+	// 	url = `https://newsapi.org/v2/everything?q=${input.value}&from=2023-03-14&sortBy=popularity&apiKey=${key}`;
+	// 	console.log(url);
+	//  } else {
+	// 	url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${key}`
+	// }
